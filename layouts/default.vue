aed0513693c2508f1e380df95936607f8ea5d964
shiguma127 <shiguma127@gmail.com>
2020-06-30 02:05:03 +0900

    ADD settings page,darkmode toggleSwitch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant="miniVariant"
@@ -26,59 +26,23 @@
     </v-navigation-drawer>
     <v-app-bar
       :clipped-left="clipped"
+      color="#2196F3"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" >
+        <v-icon color="#FFF">mdi-menu</v-icon>
+      </v-app-bar-nav-icon>
+
+      <v-toolbar-title v-text="title" class="white--text"/>
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+
     </v-app-bar>
-    <v-content>
+    <v-main>
       <v-container>
         <nuxt />
       </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    </v-main>
     <v-footer
       :absolute="!fixed"
       app
@@ -105,6 +69,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-cog',
+          title: 'settings',
+          to: '/settings'
         }
       ],
       miniVariant: false,
---
+++ pages/settings.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Settings Page</v-card-title>
+      <v-col>
+        <v-card outlined>
+          <v-row>
+            <v-col>
+              <v-card-title>DarkTheme</v-card-title>
+            </v-col>
+            <v-col>
+              <v-layout justify-end>
+                <v-switch
+                  v-model="$vuetify.theme.dark"
+                  hide-details
+                  inset
+                  color="#ffffff"
+                ></v-switch>
+              </v-layout>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-card>
+  </v-container>
+</template>
