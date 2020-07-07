f0fffea0ccbd5f4f331eb59c06299992eeb8f121
shiguma127 <shiguma127@gmail.com>
2020-07-08 00:02:44 +0900

    add realTimeChart

---
+++ components/StreamingChart.vue
@@ -0,0 +1,49 @@
+<script>
+import { Line } from 'vue-chartjs'
+import 'chartjs-plugin-streaming'
+
+export default {
+  extends: Line,
+  mounted () {
+    this.renderChart(
+      {
+        datasets: [
+          {
+            label: 'Dataset 1',
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            lineTension: 0,
+            borderDash: [8, 4]
+          },
+          {
+            label: 'Dataset 2',
+            borderColor: 'rgb(54, 162, 235)',
+            backgroundColor: 'rgba(54, 162, 235, 0.5)'
+          }
+        ]
+      },
+      {
+        scales: {
+          xAxes: [
+            {
+              type: 'realtime',
+              realtime: {
+                delay: 1000,
+                refresh: 1000,
+                onRefresh (chart) {
+                  chart.data.datasets.forEach((dataset) => {
+                    dataset.data.push({
+                      x: Date.now(),
+                      y: Math.random()
+                    })
+                  })
+                }
+              }
+            }
+          ]
+        }
+      }
+    )
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,65 @@
 <template>
-  <v-container>
-    <ChartjsDemo />
-  </v-container>
+  <v-layout column justify-center align-center>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-row >
+            <v-col>
+              <streaming-chart />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-row >
+            <v-col>
+              <streaming-chart />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-row >
+            <v-col>
+              <streaming-chart />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-card>
+          <v-row >
+            <v-col>
+              <streaming-chart />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-row >
+            <v-col>
+              <streaming-chart />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card>
+          <v-row >
+            <v-col>
+              <streaming-chart />
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-layout>
 </template>
 <script>
-const value = [10, 41, 35, 51, 49, 1, 333, 234, 23, 42, 62, 69, 91, 148]
-
-export default {
-  inject: ['theme'],
-  components: {
-    ChartjsDemo: () => import('../components/LineChart')
-  },
-  data () {
-    return {
-      series: [{
-        name: 'Desktops',
-        data: value
-      }],
-      chartOptions: {
-        chart: {
-          height: 350,
-          type: 'line',
-          zoom: {
-            enabled: true
-          }
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: 'straight'
-        },
-        title: {
-          text: 'Product Trends by Month',
-          align: 'left'
-        },
-        grid: {
-          row: {
-            colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-            opacity: 0.5
-          }
-        },
-        xaxis: {
-          range: 60,
-          labels: {
-            style: {
-              colors: ['#00FF00']
-            }
-          }
-        },
-        tooltip: {
-          theme: [
-            (this.$vuetify.theme.dark ? 'dark' : 'light')
-          ]
-        }
-      }
-    }
-  },
-  methods: {
-    func () {
-      value.push(Math.random() * 100)
-      this.$refs.chart.updateSeries([{
-        data: value
-      }])
-    }
-  }
-}
+export default {}
 </script>
