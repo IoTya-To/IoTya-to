89afb50bebe521071e0e67ca45f3ab0ae8803796
shiguma127 <shiguma127@gmail.com>
2020-07-09 14:10:49 +0900

    レスポンシブデザイン化　グラフカードの大きさcols指定

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,14 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm2 md4 lg4 xl4>
       <v-row>
-        <v-col v-for="n in 10" :key="n">
+        <v-col v-for="n in 5" :key="n" :cols="3">
           <v-card>
-            <v-row>
-              <v-col>
-                <div>
-                  <streaming-chart />
-                </div>
-              </v-col>
-            </v-row>
+            <v-card-title>
+              {{n}}
+            </v-card-title>
+            <div>
+              <streaming-chart />
+            </div>
           </v-card>
         </v-col>
       </v-row>
