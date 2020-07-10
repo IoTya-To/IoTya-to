70fb150045efb4e062c1b50e49dcaa0711063116
shiguma127 <shiguma127@gmail.com>
2020-07-10 19:48:17 +0900

    add menu btn

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,25 @@
       <v-row>
         <v-col v-for="n in 6" :key="n" :cols="4">
           <v-card>
-            <v-card-title>
-              {{ n }}
-            </v-card-title>
+            <v-row>
+              <v-col>
+                <v-card-title class="px-2 py-0 mx-3 my-0">
+                  {{ n }}
+                </v-card-title>
+              </v-col>
+              <v-spacer />
+              <v-layout align-center>
+                <v-col class="px-2 py-0 mx-3 my-0">
+                  <v-layout justify-end align-center>
+                    <v-btn class="px-2 py-0 mx-1 my-0" text icon>
+                      <v-icon>
+                        mdi-dots-vertical
+                      </v-icon>
+                    </v-btn>
+                  </v-layout>
+                </v-col>
+              </v-layout>
+            </v-row>
             <div>
               <streaming-chart />
             </div>
