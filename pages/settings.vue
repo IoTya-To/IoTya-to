ea9838d4eaa10559fc8c04ecf4f93e78e3bdfae2
shiguma127 <shiguma127@gmail.com>
2020-07-27 12:24:21 +0900

    setting.vueをcardからlistへ

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,43 +1,20 @@
 <template>
-  <v-container>
+  <v-layout justify-center>
     <v-card>
       <v-card-title>Settings Page</v-card-title>
-      <v-col>
-        <v-card outlined>
-          <v-row>
-            <v-col>
-              <v-card-title>DarkTheme</v-card-title>
-            </v-col>
-            <v-col>
-              <v-layout justify-end>
-                <v-switch
-                  v-model="$vuetify.theme.dark"
-                  hide-details
-                  inset
-                  color="#ffffff"
-                />
-              </v-layout>
-            </v-col>
-          </v-row>
-        </v-card>
-        <v-card outlined>
-          <v-row>
-            <v-col>
-              <v-card-title>DarkTheme</v-card-title>
-            </v-col>
-            <v-col>
-              <v-layout justify-end>
-                <v-switch
-                  v-model="$vuetify.theme.dark"
-                  hide-details
-                  inset
-                  color="#ffffff"
-                />
-              </v-layout>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
+      <v-list>
+        <v-list-item>
+          <v-list-item-title class="pr-16">DarkTheme</v-list-item-title>
+          <v-list-item-action>
+            <v-switch
+              v-model="$vuetify.theme.dark"
+              hide-details
+              inset
+              color="#ffffff"
+            />
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
     </v-card>
-  </v-container>
+  </v-layout>
 </template>
