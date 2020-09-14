038e67308975b8268b17eb4412ea03e48be11596
shiguma127 <shiguma127@gmail.com>
2020-09-15 00:16:42 +0900

    add LoginForm.vue

---
+++ components/LoginForm.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-col class="pa-6">
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model=Mail
+              label="e-mail"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model=Password
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'"
+              label="password"
+              @click:append="show = !show"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-layout justify-center>
+            <v-btn>
+              Login
+            </v-btn>
+          </v-layout>
+        </v-row>
+      </v-col>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  name: 'LoginForm',
+  data () {
+    return {
+      show: false,
+      Mail: '',
+      Password: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
