c91db6dab266d504eb94d23c8a7a2c9335b2b06c
shiguma127 <shiguma127@gmail.com>
2020-10-16 22:00:01 +0900

    test draggable chart

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,12 +1,19 @@
 <template>
   <v-layout>
     <v-container>
-      <Draggable class="row">
-        <v-col v-for="i in 10" :key="i" cols="3" class="ma-0">
+      <v-switch v-model="dragable" />
+      <Draggable class="row" :options="{disabled: !dragable}">
+        <v-col v-for="i in 2" :key="i" cols="3" class="ma-0">
           <v-card>
             <v-card-title>
               {{ i }}
             </v-card-title>
+            <streaming-chart v-show="!dragable" :data="chart.datasets" />
+            <v-skeleton-loader
+              v-show="dragable"
+              class="ma-auto"
+              height="400"
+            />
           </v-card>
         </v-col>
       </Draggable>
@@ -15,7 +22,29 @@
 </template>
 <script>
 import Draggable from 'vuedraggable'
+import StreamingChart from '@/components/StreamingChart'
 export default {
-  components: { Draggable }
+  components: { StreamingChart, Draggable },
+  data () {
+    return {
+      dragable: true,
+      chart: {
+        datasets: [
+          {
+            label: 'Dataset 1',
+            fill: false,
+            borderColor: '#FFFF84',
+            backgroundColor: '#FFFF84'
+          },
+          {
+            label: 'Dataset 2',
+            fill: false,
+            borderColor: '#AADDFF',
+            backgroundColor: '#AADD FF'
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
