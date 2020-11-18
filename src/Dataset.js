3899cc74585f005962508d392d4631e77ed79efc
shiguma127 <shiguma127@gmail.com>
2020-11-18 16:47:28 +0900

    add Dataset.js

---
+++ src/Dataset.js
@@ -0,0 +1,13 @@
+
+export class Dataset {
+  backgroundColor = ''
+  borderColor = ''
+  fill = true
+  label = ''
+  constructor (backgroundColor, borderColor, fill, label) {
+    this.backgroundColor = backgroundColor
+    this.borderColor = borderColor
+    this.fill = fill
+    this.label = label
+  }
+}
